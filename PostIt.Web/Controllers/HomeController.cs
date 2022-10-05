a23753bd3a00b5fd38649b232a0a5145545a373f
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2022-10-05 21:03:54 +0200

    Initial - source

---
+++ PostIt.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PostIt.Web.Models;
+
+namespace PostIt.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View("~/Views/Home/Index.cshtml");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View("~/Views/Home/Privacy.cshtml");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("~/Views/Shared/Error.cshtml",new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
---
+++ PostIt.Web/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostIt.Web.Models;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [MaxLength(25)]
+    public string Username { get; set; } = default!;
+    
+    [MaxLength(25)]
+    public string Name { get; set; } = default!;
+
+    public string PasswordHash { get; set; } = default!;
+    public string Salt { get; set; } = default!;
+}
