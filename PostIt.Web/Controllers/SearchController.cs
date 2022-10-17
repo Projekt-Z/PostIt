9395f2a12a30e86f3a44e195a53aa4629e0e37a4
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2022-10-17 23:36:25 +0200

    + Searching🔎

---
+++ PostIt.Web/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostIt.Web.Data;
+using PostIt.Web.Services.Posts;
+
+namespace PostIt.Web.Controllers;
+
+[Route("[controller]")]
+public class SearchController : Controller
+{
+    private readonly IPostService _postService;
+
+    public SearchController(IPostService postService)
+    {
+        _postService = postService;
+    }
+    
+    public IActionResult Search([FromQuery] string q)
+    {
+        var posts = _postService.GetAll();
+        
+        foreach (var post in posts)
+        {
+            if (post.Title.Contains(q) || post.Description.Contains(q))
+            {
+                return View(posts);
+            }
+        }
+        
+        return View();
+    }
+}
